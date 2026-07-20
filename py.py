9ee6c1df0fb196d3d64a923b91ae396a91338977
icoderedmond-afk <icoderedmond@gmail.com>
2026-07-20 13:07:04 -0700

    Add files via upload

---
+++ py.py
@@ -0,0 +1,14 @@
+choice = input("what is 1 + 1?")
+
+
+if choice == "2":
+    print("ur dumb")
+
+
+elif choice == "3":
+    print("OMG UR SMARTER THAN GORDON RAMSEY. actually idk if gordon smart.")
+
+
+
+else:
+    print("very smart. VERY VERY VERY VERY VERY VERY VERY VERY VERY VERY VERY VERY VERY VERY SMART!!!!!!")
